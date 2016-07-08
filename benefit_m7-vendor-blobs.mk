f6f9c86c0f470d8280223d053dcff39807157c17
olegsvs <oleg.texet@gmail.com>
2016-07-08 10:35:26 -0900

    fix typo and touch

--- benefit_m7-vendor-blobs.mk
+++ benefit_m7-vendor-blobs.mk
@@ -1,18 +1,16 @@
 # copy files
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/benefit_m7/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ark/benefit_m7/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/benefit_m7/lib64,system/lib64)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ark/benefit_m7/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/benefit_m7/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ark/benefit_m7/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/benefit_m7/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ark/benefit_m7/usr,system/usr)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/benefit_m7/usr,system/usr)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ark/benefit_m7/xbin,system/xbin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/benefit_m7/xbin,system/xbin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/benefit_m7/vendor/lib,system/vendor/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ark/benefit_m7/vendor/lib,system/vendor/lib)
 
 # copy files
 
