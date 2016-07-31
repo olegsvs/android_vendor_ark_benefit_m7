3c4a087a7ab24ef86a1e8b97a590f7a50d7ce468
olegsvs <oleg.texet@gmail.com>
2016-07-31 15:56:47 -0400

    add mk

---
+++ benefit_m7-vendor-blobs.mk
@@ -0,0 +1,19 @@
+# copy files
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ark/benefit_m7/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ark/benefit_m7/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ark/benefit_m7/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ark/benefit_m7/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ark/benefit_m7/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ark/benefit_m7/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ark/benefit_m7/vendor,system/vendor)
+
+# copy files
+
+
